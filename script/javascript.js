f14d4a0bfa74045b71aba7dd785a19ff33883d8a
MhicoTumbaga018 <mhico.tumbaga.018@gmail.com>
2026-08-15 16:01:08 +0800

    task for rewritten with arrow functions

--- script/javascript.js
+++ script/javascript.js
@@ -1,11 +1,29 @@
-function min(a, b) {
-  if (a < b) {
-    return a;
-  } else {
-    return b;
-  }
-}
+let double = (n) => n * 2;
+// roughly the same as: let double = function(n) { return n * 2 }
+alert(double(3)); // 6
 
-console.log(min(10, 5));
-console.log(min(-10, -20));
-console.log(min(100, 100));
+let sum = (a, b) => a + b;
+
+/* This arrow function is a shorter form of:
+
+let sum = function(a, b) {
+  return a + b;
+};
+*/
+
+alert(sum(1, 2)); // 3
+
+let sayHi = () => alert("Hello!");
+sayHi();
+
+let age = prompt("What is your age?");
+let welcome = age < 18 ? () => alert("Hello!") : () => alert("Greetings!");
+welcome();
+
+let sum = (a, b) => {
+  // the curly brace opens a multiline function
+  let result = a + b;
+  return result; // if we use curly braces, then we need an explicit "return"
+};
+
+alert(sum(1, 2)); // 3
