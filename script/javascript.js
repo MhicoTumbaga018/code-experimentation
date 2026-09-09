85bda77171ab3d6eb252276e656e3697c682948c
MhicoTumbaga018 <mhico.tumbaga.018@gmail.com>
2026-09-09 16:33:37 +0800

    Simple for loop in array

--- script/javascript.js
+++ script/javascript.js
@@ -1,23 +1,5 @@
-/** When a user inputs a number*/
-let answer = parseInt(
-  prompt("Please enter the number you would like to FizzBuzz up to:"),
-);
+const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
 
-/** Loop from 1 to the entered number */
-
-for (let i = 1; i <= answer; i++) {
-  /**If the current number is divisible by 3 and 5 then print "FizzBuzz" */
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
-  } else if (i % 3 === 0) {
-    /** If the current number is divisible by 3 then print "Fizz" */
-    console.log("Fizz");
-
-    /**If the current number is divisible by 5 then print "Buzz" */
-  } else if (i % 5 == 0) {
-    console.log("Buzz");
-    /**Otherwise print the current number */
-  } else {
-    console.log(i);
-  }
+for (const cat of cats) {
+  console.log(cat);
 }
