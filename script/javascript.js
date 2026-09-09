3d98f4f4476e9c4c0eef3d04e1478053c4c26f86
MhicoTumbaga018 <mhico.tumbaga.018@gmail.com>
2026-09-09 20:38:32 +0800

    for loop with map object

--- script/javascript.js
+++ script/javascript.js
@@ -1,5 +1,14 @@
+function toUpper(string) {
+  return string.toUpperCase();
+}
+
 const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
 
 for (const cat of cats) {
   console.log(cat);
 }
+
+const upperCats = cats.map(toUpper);
+
+console.log(upperCats);
+// [ "LEOPARD", "SERVAL", "JAGUAR", "TIGER", "CARACAL", "LION" ]
