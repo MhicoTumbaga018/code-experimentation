ab18dc0b5f63bd64fa55874240b168ee8ecd41fe
MhicoTumbaga018 <mhico.tumbaga.018@gmail.com>
2026-09-17 00:20:43 +0800

    Looping through collections with a for loop with cleaner version

--- script/javascript.js
+++ script/javascript.js
@@ -1,5 +1,14 @@
-const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
+const cats = ["Pete", "Biggles", "Jasmine"];
+
+let myFavoriteCats = "My cats are called ";
 
 for (let i = 0; i < cats.length; i++) {
-  console.log(cats[i]);
+  if (i === cats.length - 1) {
+    myFavoriteCats += `and ${cats[i]}. `;
+  } else {
+    myFavoriteCats += `${cats[i]}, `;
+  }
 }
+
+console.log(myFavoriteCats);
+// "My cats are called Pete, Biggles, Jasmine. "
